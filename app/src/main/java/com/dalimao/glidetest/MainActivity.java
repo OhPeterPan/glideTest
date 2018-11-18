9b34c131aa22c4a0d1dc1feab20fe32cd8760a45
wuaoke <987603594@qq.com>
2018-11-18 18:12:27 +0800

    真正加载

--- app/src/main/java/com/dalimao/glidetest/MainActivity.java
+++ app/src/main/java/com/dalimao/glidetest/MainActivity.java
@@ -1,13 +1,32 @@
 package com.dalimao.glidetest;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+import com.bumptech.glide.Glide;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     String url = "http://cn.bing.com/az/hprichbg/rb/Dongdaemun_ZH-CN10736487148_1920x1080.jpg";
+    private ImageView iv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        Button button = findViewById(R.id.button);
+        iv = findViewById(R.id.iv);
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Glide.with(this).load(url).into(iv);
     }
 }
