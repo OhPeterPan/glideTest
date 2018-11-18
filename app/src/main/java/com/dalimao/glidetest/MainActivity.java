bbff7a8feaa9664089ed88e31cbdb9c49e90ca37
wuaoke <987603594@qq.com>
2018-11-18 17:15:12 +0800

    第一次提交

---
+++ app/src/main/java/com/dalimao/glidetest/MainActivity.java
@@ -0,0 +1,13 @@
+package com.dalimao.glidetest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    String url = "http://cn.bing.com/az/hprichbg/rb/Dongdaemun_ZH-CN10736487148_1920x1080.jpg";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
